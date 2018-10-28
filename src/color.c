cee30f95d8603180d34823425d8604371920935a
rayane Amrouche <=>
2018-10-28 05:58:06 +0100

    get the color of the object

--- src/color.c
+++ src/color.c
@@ -1,7 +1,9 @@
 #include "color.h"
 
-struct pixel get_object_color(struct scene *s, struct hit hit)
+struct pixel get_object_color(struct scene *s, struct hit hit, vec3_t light)
 {
     struct material mat = s->mtls[hit.obj.mtl_id];
-    return init_pixel(mat.diffuse.x * 255, mat.diffuse.y * 255, mat.diffuse.z * 255);
+    return init_pixel(light.x * mat.diffuse.x * 255,
+                      light.y * mat.diffuse.y * 255,
+                      light.z * mat.diffuse.z * 255);
 }
--- src/color.h
+++ src/color.h
@@ -1,9 +1,9 @@
 #ifndef COLOR_H
 #define COLOR_H
 
-#include "struct.h"
 #include "img.h"
+#include "struct.h"
 
-struct pixel get_object_color(struct scene *s, struct hit hit);
+struct pixel get_object_color(struct scene *s, struct hit hit, vec3_t light);
 
 #endif /* COLOR_H */
